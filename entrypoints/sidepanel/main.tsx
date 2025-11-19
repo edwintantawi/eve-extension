70ac5182c8d900dbae01c4bcb6e7d626b48635b2
edwintantawi <edwintantawi.dev@gmail.com>
2025-11-19 21:53:58 +0700

    feat: add side panel

--- entrypoints/sidepanel/main.tsx
+++ entrypoints/sidepanel/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "@/entrypoints/popup/app";
+import { App } from "@/entrypoints/sidepanel/app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  manifest: {
+    action: {},
+  },
   // @ts-expect-error
   vite: () => ({
     plugins: [tailwindcss()],
